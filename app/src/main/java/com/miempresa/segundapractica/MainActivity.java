54a46cf21798ac333dbbe11fc37bb2d6cca1817e
juanp <juanpabloj32@gmail.com>
2026-05-20 21:07:58 -0600

    Se le agrego la pantalla de VentasFragment y el boton de menu

--- app/src/main/java/com/miempresa/segundapractica/MainActivity.java
+++ app/src/main/java/com/miempresa/segundapractica/MainActivity.java
@@ -2,23 +2,55 @@ package com.miempresa.segundapractica;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        // Pantalla inicial
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, new VentasFragment())
+                    .commit();
+        }
+
+        // Solo dejar funcionando VentasFragment
+        bottomNavigation.setOnItemSelectedListener(item -> {
+
+            Fragment fragment = null;
+
+            int id = item.getItemId();
+
+            if (id == R.id.menu_ventas) {
+
+                fragment = new VentasFragment();
+            }
+
+            if (fragment != null) {
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainer, fragment)
+                        .commit();
+
+                return true;
+            }
+
+            return false;
         });
     }
 }
