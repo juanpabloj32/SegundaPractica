fca649c9b71c48cef40edb6bed0f5535d9f0d3b7
juanp <juanpabloj32@gmail.com>
2026-05-20 20:53:05 -0600

    Se le agrego la pantalla de VentasFragment y el boton de menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.miempresa.segundapractica"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.miempresa.segundapractica"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
